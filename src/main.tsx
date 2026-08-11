9c35b165b63abc1e10cae6fa876235cc16eca1ad
Selim Öztürk <s.ozturk4961@gmail.com>
2026-08-11 14:37:23 +0300

    chore: install and configure TakeoffUI

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { TakeoffSparProvider } from '@takeoff-ui/react-spar'
+import '@takeoff-design/tokens/css/default/theme.css'
 import './index.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <TakeoffSparProvider colorMode="light">
+      <App />
+    </TakeoffSparProvider>
   </StrictMode>,
 )
